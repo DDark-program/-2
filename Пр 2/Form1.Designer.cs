ef14687be1c3179582009b7428b8da4c8fdd1251
DDark-program <vjames_kingv@mail.ru>
2020-11-10 10:26:31 +0300

    Добавьте файлы проекта.

---
+++ Пр 2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Пр_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFill = new System.Windows.Forms.Button();
+            this.NumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.btdLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProizv = new System.Windows.Forms.TextBox();
+            this.Lbl1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.обАвтореToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заданиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(201, 254);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(152, 58);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Расчитать";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnFill);
+            this.panel1.Controls.Add(this.NumUpDown);
+            this.panel1.Controls.Add(this.btdLoad);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.Table);
+            this.panel1.Controls.Add(this.btnInfo);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtProizv);
+            this.panel1.Controls.Add(this.Lbl1);
+            this.panel1.Controls.Add(this.btnCalc);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(408, 569);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(45, 254);
+            this.btnFill.Margin = new System.Windows.Forms.Padding(5);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(152, 58);
+            this.btnFill.TabIndex = 16;
+            this.btnFill.Text = "Заполнить";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // NumUpDown
+            // 
+            this.NumUpDown.Location = new System.Drawing.Point(55, 66);
+            this.NumUpDown.Name = "NumUpDown";
+            this.NumUpDown.Size = new System.Drawing.Size(298, 29);
+            this.NumUpDown.TabIndex = 15;
+            this.NumUpDown.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.NumUpDown.ValueChanged += new System.EventHandler(this.NumUpDown_ValueChanged);
+            // 
+            // btdLoad
+            // 
+            this.btdLoad.Location = new System.Drawing.Point(201, 370);
+            this.btdLoad.Margin = new System.Windows.Forms.Padding(5);
+            this.btdLoad.Name = "btdLoad";
+            this.btdLoad.Size = new System.Drawing.Size(152, 58);
+            this.btdLoad.TabIndex = 13;
+            this.btdLoad.Text = "Открыть";
+            this.btdLoad.UseVisualStyleBackColor = true;
+            this.btdLoad.Click += new System.EventHandler(this.btdLoad_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(45, 370);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(152, 58);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(45, 506);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(308, 58);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Table
+            // 
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table.Location = new System.Drawing.Point(3, 96);
+            this.Table.Name = "Table";
+            this.Table.Size = new System.Drawing.Size(403, 150);
+            this.Table.TabIndex = 10;
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(201, 438);
+            this.btnInfo.Margin = new System.Windows.Forms.Padding(5);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(152, 58);
+            this.btnInfo.TabIndex = 9;
+            this.btnInfo.Text = "О программе";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(45, 438);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(152, 58);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 317);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Результат";
+            // 
+            // txtProizv
+            // 
+            this.txtProizv.Location = new System.Drawing.Point(45, 331);
+            this.txtProizv.Margin = new System.Windows.Forms.Padding(5);
+            this.txtProizv.Name = "txtProizv";
+            this.txtProizv.ReadOnly = true;
+            this.txtProizv.Size = new System.Drawing.Size(308, 29);
+            this.txtProizv.TabIndex = 4;
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.Location = new System.Drawing.Point(109, 42);
+            this.Lbl1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(183, 21);
+            this.Lbl1.TabIndex = 2;
+            this.Lbl1.Text = "Введите кол-во чисел";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.обАвтореToolStripMenuItem,
+            this.заданиеToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(408, 31);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // обАвтореToolStripMenuItem
+            // 
+            this.обАвтореToolStripMenuItem.Name = "обАвтореToolStripMenuItem";
+            this.обАвтореToolStripMenuItem.Size = new System.Drawing.Size(104, 25);
+            this.обАвтореToolStripMenuItem.Text = "Об авторе";
+            this.обАвтореToolStripMenuItem.Click += new System.EventHandler(this.обАвтореToolStripMenuItem_Click);
+            // 
+            // заданиеToolStripMenuItem
+            // 
+            this.заданиеToolStripMenuItem.Name = "заданиеToolStripMenuItem";
+            this.заданиеToolStripMenuItem.Size = new System.Drawing.Size(86, 25);
+            this.заданиеToolStripMenuItem.Text = "Задание";
+            this.заданиеToolStripMenuItem.Click += new System.EventHandler(this.заданиеToolStripMenuItem_Click);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(75, 25);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 568);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Практическая 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label Lbl1;
+        private System.Windows.Forms.TextBox txtProizv;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem обАвтореToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заданиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.DataGridView Table;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btdLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.NumericUpDown NumUpDown;
+        private System.Windows.Forms.Button btnFill;
+    }
+}
+
